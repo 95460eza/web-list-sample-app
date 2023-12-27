ea9a360aaab53d135e1b389f67c2393a4be788ee
95460eza <alfredkoffisokpa@gmail.com>
2023-12-26 22:46:33 -0500

    Removed mangum

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import mangum
+#from keras_prediction_via_web_app import flask_web_app
+#import mangum
 from mangum import Mangum
 
-from keras_prediction_via_web_app import flask_web_app
 
-lambda_handler=Mangum(flask_web_app)
+#lambda_handler=Mangum(flask_web_app)
 
 if __name__ == '__main__':
     flask_web_app.run(host='0.0.0.0', port=8000, debug=True)
